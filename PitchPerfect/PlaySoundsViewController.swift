a48baf0a1f3fce4f136b79d49934f3b98ab8170a
Ayush Garg <ayushgarg@Ayushs-MacBook-Pro.local>
2016-10-17 02:48:02 +0530

    set buttons content mode to scaleAspectFit

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -60,6 +60,13 @@ class PlaySoundsViewController: UIViewController {
         setupAudio()
         configureUI(playState: .NotPlaying)
         // Do any additional setup after loading the view.
+        snailButton.imageView?.contentMode = UIViewContentMode.scaleAspectFit
+        rabbitButton.imageView?.contentMode = UIViewContentMode.scaleAspectFit
+        chipmunkButton.imageView?.contentMode = UIViewContentMode.scaleAspectFit
+        vaderButton.imageView?.contentMode = UIViewContentMode.scaleAspectFit
+        reverbButton.imageView?.contentMode = UIViewContentMode.scaleAspectFit
+        echoButton.imageView?.contentMode = UIViewContentMode.scaleAspectFit
+        stopButton.imageView?.contentMode = UIViewContentMode.scaleAspectFit
     }
 
 }
